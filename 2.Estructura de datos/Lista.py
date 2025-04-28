3b95e0d0e06f811cc3b6fca9d20f566d19e678ce
Jeffercastil <122533549+Jeffercastil@users.noreply.github.com>
2025-04-28 13:41:48 -0500

    update 2.Estructura de datos/Lista.py

---
+++ 2.Estructura de datos/Lista.py
@@ -0,0 +1,22 @@
+cantidad=int (input(f" ingresar la cantidad de valores:"))
+lista=[]
+for i range (cantidad):
+   valor =int(input(f" ingrese el valor {i+1} :"))
+   lista.append(valor)
+   
+   if lista:
+     total =sum(lista)
+     promedio = total / len(lista)
+     maximo= max(lista)
+     minimo = min (lista)
+     
+     print(" Resumen estadistico")
+     print(f" Total de ventas: {total}")
+     print(f"promedio de ventas :{promedio:.2f}")
+     print(f"Maximo: {maximo}")
+     print(f"Minimo: {minimo}")
+    else:
+      print("No se ingresaron datos")
+    
+  
+
