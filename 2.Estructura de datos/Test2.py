716c07421dafa74267e79cee5f317dd64af0bc97
Jeffercastil <122533549+Jeffercastil@users.noreply.github.com>
2025-04-30 07:09:10 -0500

    update 2.Estructura de datos/Test2.py

---
+++ 2.Estructura de datos/Test2.py
@@ -0,0 +1,13 @@
+n=int(input("cuantas temperaturas deseas ingresar:"))
+temperaturas={}
+for i in range(n)
+temp=float(input(f"ingresa temperatura {i+1} :"))
+temperaturas.append(temp)
+
+umbral=float(input("\Ingrese el umbral para temperaturas altas :"))
+altas_temperaturas=[temp for temp in temperaturas if temp >umbral ]
+
+print ("\Resultados:")
+print(f"Temperaturas orinales:{Temperaturas}")
+
+
