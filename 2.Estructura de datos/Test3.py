5c6a57090a4c27a5619fc3500b30b0a74c6efcc6
Jeffercastil <122533549+Jeffercastil@users.noreply.github.com>
2025-05-04 18:18:01 -0500

    update 2.Estructura de datos/Test3.py

--- 2.Estructura de datos/Test3.py
+++ 2.Estructura de datos/Test3.py
@@ -1,23 +1,25 @@
-n=int(input("Cuantos clientes desea agregar?"))
-clientes=[]
+# Solicitar cantidad de clientes a registrar
+n = int(input("¿Cuántos clientes deseas registrar? "))
+clientes = []
 
+# Ingresar datos de cada cliente
 for i in range(n):
-  print(f"\Ingresar datos del cliente {i+1}")
-  nombre=input("Nombre:")
-  Compras=int(input("Numeros de compras: "))
-  gasto_total=float(input("Gasto total:"))
-  
-  cliente.append({
-    "id":i+1,
-    "Nombre",nombre,
-    "Compras",Compras,
-    "Gasto total",gasto_total
-  })
-  
-  print("\nResumen de clientes:")
-  for cliente in clientes:
-    gasto_promedio=
-    cliente['gasto_total']/cliente['compras']
-    print(f"{cliente['Nombre']}:
-      {cliente['compras']}compras,"f"Gastos total:${ cliente['gasto_total']:.2f},"f"Promedio:${gasto_promedio:.2f} por comprar ")
+    print(f"\nIngresa datos del cliente {i+1}:")
+    nombre = input("Nombre: ")
+    compras = int(input("Número de compras: "))
+    gasto_total = float(input("Gasto total: $"))
     
+    clientes.append({
+        "id": i+1,
+        "nombre": nombre,
+        "compras": compras,
+        "gasto_total": gasto_total
+    })
+
+# Calcular gasto promedio por compra
+print("\nResumen de clientes:")
+for cliente in clientes:
+    gasto_promedio = cliente['gasto_total'] / cliente['compras']
+    print(f"{cliente['nombre']}: {cliente['compras']} compras, "
+          f"Gasto total: ${cliente['gasto_total']:.2f}, "
+          f"Promedio: ${gasto_promedio:.2f} por compra")
