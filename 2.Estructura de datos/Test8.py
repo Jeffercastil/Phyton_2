e901c22c60c7c5dfbed9b38fc7e36841abce68f2
Jeffercastil <122533549+Jeffercastil@users.noreply.github.com>
2025-05-08 16:51:59 -0500

    update 2.Estructura de datos/Test8.py

---
+++ 2.Estructura de datos/Test8.py
@@ -0,0 +1,17 @@
+texto_guardados=[]
+
+while true:
+  print("\n--Menu---")
+  print(" 1. Agregar texto")
+  print(" 2. Mostrar todos los twxtos en orden")
+  print(" Salir")
+  
+  opcion=input ("selecione una opcion (1-3")
+  
+  if(opcion == "1":
+    nuevo_texto=input("\n Ingresar el texto que deseas guardar: ")
+    texto_guardados.append(nuevo_texto)
+    print(f"texto guardado correctamente".Total:{len(texto_guardados)})
+    )
+  
+  
