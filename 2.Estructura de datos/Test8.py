4118f0988e8ffae9c7b88cc256dc36735bcaf9c2
Jeffercastil <122533549+Jeffercastil@users.noreply.github.com>
2025-05-12 18:14:38 -0500

    update 2.Estructura de datos/Test8.py

--- 2.Estructura de datos/Test8.py
+++ 2.Estructura de datos/Test8.py
@@ -1,6 +1,6 @@
 texto_guardados=[]
 
-while true:
+while True:
   print("\n--Menu---")
   print(" 1. Agregar texto")
   print(" 2. Mostrar todos los textos en orden")
@@ -8,23 +8,23 @@ while true:
   
   opcion=input ("selecione una opcion (1-3")
   
-  if(opcion == "1":
+  if opcion == "1":
     nuevo_texto=input("\n Ingresar el texto que deseas guardar: ")
     texto_guardados.append(nuevo_texto)
-    print(f"texto guardado correctamente".Total:{len(texto_guardados)})
-    )
-    elif opcion =="2":
+    print(f"texto guardado correctamente.Total:{len(texto_guardados)} ")
+    
+  elif opcion =="2":
       if not texto_guardados:
         print ("\n No hay textos guardados aun")
-        else:
+      else:
           print("\n Texto guardado")
           for i,texto in enumerate(texto_guardados,1):
             print(f"{i}.{texto}")
             
-    elif opcion == "3":
+  elif opcion == "3":
       print("\n Saliendo del programa ")
       break
-    else:
+  else:
       print("\n Opcion no valida intenta otra vez")
         
   
