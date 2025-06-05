4d48e64bb11a811fef685398b7695d327fd2bdef
Jeffercastil <122533549+Jeffercastil@users.noreply.github.com>
2025-06-04 20:49:58 -0500

    update 2.Estructura de datos/herencia y polimorfismo.py

--- 2.Estructura de datos/herencia y polimorfismo.py
+++ 2.Estructura de datos/herencia y polimorfismo.py
@@ -10,4 +10,22 @@ class DataSetPlus(DataSet):
   def desviacion_estandar(self):
     nu = self(media)
     return(sum (x - nu ) ** 2 for x in self.data) / self.tamanho ) ** 0.5
+    
+    
+datos=[]
+datosplus=[]
+
+ta=int(input("cuantos valores desea ingresar en datos"))
+
+for i in range(ta):
+  va =float(input(f"Ingresar valores de datos :{i+1}"))
+  datos.append(va)
+  
+tb=int(input("Cuantos valores desea ingresar en dataplus"))
+
+for i in range(tb):
+  vb=float(input(f"Ingresar valores de dataplus : {i+1}"))
+  datosplus.append(vb)
+  
+print(f"los valores de datos  es: {datos} ,Los valores de dataplus:{datosplus}")
   
