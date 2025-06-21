b07306c943a5733b05d35a9ff22e0f8fcb2d414d
Jeffercastil <122533549+Jeffercastil@users.noreply.github.com>
2025-06-20 19:58:49 -0500

    update 2.Estructura de datos/Atributos_metodos.py

---
+++ 2.Estructura de datos/Atributos_metodos.py
@@ -0,0 +1,22 @@
+Mis_datos =[]
+
+valor=int(input("cuantos valores deseas agregar"))
+for i in range (valor):
+  va = int (input(f"ingresa los valores :{i+1}"))
+  Mis_datos.append(va)
+  
+class DataSet:
+  def __init__(self,data):
+    self.data = data
+  
+  def media(self):
+    return  sum (self.data) / len(self.data)
+    
+  def mediana(self):
+    sorted_data = sorted(self.data)
+    n = len(sorted_data)
+    midpoint = n // 2
+    if n % 2 == 0:
+      return ( sorted_data [midpoint - 1 ]+ sorted_data[midpoint]) / 2
+    
+    
