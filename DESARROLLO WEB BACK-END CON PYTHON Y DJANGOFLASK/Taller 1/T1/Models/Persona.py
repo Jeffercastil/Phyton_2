5b7d079dc91894887d7b8c814f1b8c80558a55f1
Jeffercastil <122533549+Jeffercastil@users.noreply.github.com>
2025-08-29 14:11:56 -0500

    update DESARROLLO WEB BACK-END CON PYTHON Y DJANGOFLASK/Taller 1/T1/Models/Persona.py

--- DESARROLLO WEB BACK-END CON PYTHON Y DJANGOFLASK/Taller 1/T1/Models/Persona.py
+++ DESARROLLO WEB BACK-END CON PYTHON Y DJANGOFLASK/Taller 1/T1/Models/Persona.py
@@ -1,14 +1,23 @@
 class persona:
-    def __init__(self, nombre, apellido, edad, sexo, telefono, email, direccion, ciudad, pais):
+    def __init__(self, nombre: str, edad:int):
         self.nombre = nombre
-        self.apellido = apellido
         self.edad = edad
-        self.sexo = sexo
-        self.telefono = telefono
-        self.email = email
-        self.direccion = direccion
-        self.ciudad = ciudad
-        self.pais = pais
+
 
     def Presentarse(self):
-        print(f"Nombre: {self.nombre}\nApellido: {self.apellido}\nEdad: {self.edad}\nSexo: {self.sexo}\nTelefono: {self.telefono}\nEmail: {self.email}\nDireccion: {self.direccion}\nCiudad: {self.ciudad}\nPais: {self.pais}")
+        print(f"Hola soy {self.nombre} y tengo {self.edad} anhos.")
+
+class adoptante(persona):
+  def __init__(self,nombre:str,edad:int):
+    super().__init__(nombre,edad)
+    self.mascotas_adoptadas=[]
+    
+  def adoptar(self,mascota:Mascota):
+    if mascota.adoptado:
+      return f"La mascota {mascota.nombre} ya fue adoptado"
+    mascota.adoptado =True
+    self.mascotas_adoptadas.append(mascota)
+    return f"{self.nombre} ha adoptado a {mascota.nombre}."
+    
+  
+    
