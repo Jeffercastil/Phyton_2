2885b782a8630a59f8de5079b6a476fcdf841301
Jeffercastil <122533549+Jeffercastil@users.noreply.github.com>
2025-09-03 20:08:27 -0500

    update DESARROLLO WEB BACK-END CON PYTHON Y DJANGOFLASK/Taller 1/T1/Models/Main.py

---
+++ DESARROLLO WEB BACK-END CON PYTHON Y DJANGOFLASK/Taller 1/T1/Models/Main.py
@@ -0,0 +1,9 @@
+from models.mascota import Mascotas
+from models.persona import Personas
+from models.refugio import refugio
+
+
+refugio =refugio()
+cantidad = int(input("¿Cuántas mascotas deseas registrar? "))
+
+
