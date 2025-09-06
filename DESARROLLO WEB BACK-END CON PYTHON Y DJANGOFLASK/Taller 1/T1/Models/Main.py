334fdcba9452f77182471724bd0618263c2233be
Jeffercastil <122533549+Jeffercastil@users.noreply.github.com>
2025-09-06 01:55:59 -0500

    update DESARROLLO WEB BACK-END CON PYTHON Y DJANGOFLASK/Taller 1/T1/Models/Main.py

--- DESARROLLO WEB BACK-END CON PYTHON Y DJANGOFLASK/Taller 1/T1/Models/Main.py
+++ DESARROLLO WEB BACK-END CON PYTHON Y DJANGOFLASK/Taller 1/T1/Models/Main.py
@@ -5,5 +5,8 @@ from models.refugio import refugio
 
 refugio =refugio()
 cantidad = int(input("¿Cuántas mascotas deseas registrar? "))
-
-
+for i in range cantidad:
+  print(f"Mascotas N{i+1}")
+  nombre=input("Nombre:")
+  especie=input("Especie:")
+  edad=int(input("Edad: "))
